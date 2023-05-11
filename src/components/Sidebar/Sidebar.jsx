e5d3fd9f49ad34eb2a80b0ecf972b0513582e4c2
YuraKovalevich <kovalevichyura@gmail.com>
2023-05-12 00:49:41 +0300

    add sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import styles from '../../styles/Sidebar.module.css'
 import {NavLink} from "react-router-dom";
+import {useSelector} from "react-redux";
 const Sidebar = () => {
+    const {list}=useSelector(({categories})=>
+        categories
+    )
     return (
         <section className={styles.sidebar}>
             <div className={styles.title}>CATEGORIES</div>
             <nav>
                 <ul className={styles.menu}>
-                    <li>
-                        <NavLink to={`/categories/${1}`}></NavLink>
-                    </li>
+                    {list.map(({id,name})=>(
+                        <li key={id}>
+                            <NavLink
+                                className={({isActive})=>`${styles.link} ${isActive?styles.active:''}`}
+                                to={`/categories/${id}`}>{name}</NavLink>
+                        </li>
+                    ))}
                 </ul>
             </nav>
-            <div>
-                <a href="/" className={styles.link}>Help</a>
+            <div className={styles.footer}>
+                <a href="/" target='_blank' className={styles.link}>Help</a>
                 <a href="/" className={styles.link} style={{textDecoration:"underline"}}>Terms & Conditions</a>
             </div>
         </section>
