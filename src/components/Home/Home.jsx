ec5d1a09f62dedb4c2783c60e9fef2759ebb0fc9
YuraKovalevich <kovalevichyura@gmail.com>
2023-05-17 19:59:09 +0300

    add poster

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Poster from "../Poster/Poster";
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Poster/>
         </div>
     );
 };
